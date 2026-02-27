4f99f2f812ab4c73256538b836a685a70aa70a52
ProWhale123 <arpadbretz@gmail.com>
2026-02-27 23:36:16 +0100

    fix: add use client to page.tsx to support ssr: false

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import dynamic from "next/dynamic";
+"use client";
 
+import dynamic from "next/dynamic";
 const EchoExperience = dynamic(() => import("@/components/EchoExperience"), {
   ssr: false,
 });
